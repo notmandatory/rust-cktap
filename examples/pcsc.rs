7cb971631a12db4b5f1f1156a62f3ffef26c9776
Steve Myers <steve@notmandatory.org>
2023-01-25 23:11:01 -0600

    Add Error handling

--- examples/pcsc.rs
+++ examples/pcsc.rs
@@ -1,77 +1,89 @@
 extern crate core;
 
-use pcsc::{Context, Error, MAX_BUFFER_SIZE, Protocols, Scope, ShareMode};
-use rust_cktap::{AppletSelect, CommandApdu, ResponseApdu, StatusCommand, StatusResponse};
+use pcsc::{Context, Protocols, Scope, ShareMode, MAX_BUFFER_SIZE};
+use rust_cktap::{
+    AppletSelect, CommandApdu, Error, ResponseApdu, StatusCommand, StatusResponse, WaitCommand,
+    WaitResponse,
+};
 
-fn main() {
+fn main() -> Result<(), Error> {
     // Establish a PC/SC context.
-    let ctx = match Context::establish(Scope::User) {
-        Ok(ctx) => ctx,
-        Err(err) => {
-            eprintln!("Failed to establish context: {}", err);
-            std::process::exit(1);
-        }
-    };
+    let ctx = Context::establish(Scope::User)?;
 
     // List available readers.
     let mut readers_buf = [0; 2048];
-    let mut readers = match ctx.list_readers(&mut readers_buf) {
-        Ok(readers) => readers,
-        Err(err) => {
-            eprintln!("Failed to list readers: {}", err);
-            std::process::exit(1);
-        }
-    };
+    let mut readers = ctx.list_readers(&mut readers_buf)?;
 
     // Use the first reader.
     let reader = match readers.next() {
-        Some(reader) => reader,
+        Some(reader) => Ok(reader),
         None => {
-            println!("No readers are connected.");
-            return;
+            //println!("No readers are connected.");
+            Err(Error::PcSc("No readers are connected.".to_string()))
         }
-    };
+    }?;
     println!("Using reader: {:?}\n", reader);
 
     // Connect to the card.
-    let card = match ctx.connect(reader, ShareMode::Shared, Protocols::ANY) {
-        Ok(card) => card,
-        Err(Error::NoSmartcard) => {
-            println!("A smartcard is not present in the reader.");
-            return;
-        }
-        Err(err) => {
-            eprintln!("Failed to connect to card: {}", err);
-            std::process::exit(1);
-        }
-    };
+    let card = ctx.connect(reader, ShareMode::Shared, Protocols::ANY)?;
 
     // Send ISO App Select.
     let applet_select_apdu = AppletSelect::default().apdu_bytes();
-    println!("Sending 'ISO Applet Select' APDU: {:?}\n", &applet_select_apdu);
+    println!(
+        "Sending 'ISO Applet Select' APDU: {:?}\n",
+        &applet_select_apdu
+    );
     let mut rapdu_buf = [0; MAX_BUFFER_SIZE];
-    let rapdu = match card.transmit(&applet_select_apdu.as_slice(), &mut rapdu_buf) {
-        Ok(rapdu) => rapdu,
-        Err(err) => {
-            eprintln!("Failed to transmit APDU command to card: {}", err);
-            std::process::exit(1);
-        }
-    };
-    let status_response = StatusResponse::from_cbor(rapdu.to_vec());
+    let rapdu = card.transmit(&applet_select_apdu.as_slice(), &mut rapdu_buf)?;
+    let status_response = StatusResponse::from_cbor(rapdu.to_vec())?;
     println!("Received 'Status' APDU: {:?}\n", status_response);
 
-    // Send 'Status' Request.
+    // Send 'Status' Command.
     let status_apdu = StatusCommand::default().apdu_bytes();
     println!("Sending 'Status' APDU: {:?}\n", &status_apdu);
     let mut rapdu_buf = [0; MAX_BUFFER_SIZE];
-    let rapdu = match card.transmit(&status_apdu.as_slice(), &mut rapdu_buf) {
-        Ok(rapdu) => rapdu,
-        Err(err) => {
-            eprintln!("Failed to transmit APDU command to card: {}", err);
-            std::process::exit(1);
-        }
-    };
-
-    let status_response = StatusResponse::from_cbor(rapdu.to_vec());
+    let rapdu = card.transmit(&status_apdu.as_slice(), &mut rapdu_buf)?;
+    let status_response = StatusResponse::from_cbor(rapdu.to_vec())?;
     println!("Received 'Status' APDU: {:?}\n", status_response);
+
+    // Send 'Wait' Command.
+    let wait_apdu = WaitCommand::new(None, None).apdu_bytes();
+    println!("Sending 'Wait' APDU: {:?}\n", &wait_apdu);
+    let mut rapdu_buf = [0; MAX_BUFFER_SIZE];
+    let rapdu = card.transmit(&wait_apdu.as_slice(), &mut rapdu_buf)?;
+    let wait_response = WaitResponse::from_cbor(rapdu.to_vec())?;
+    println!("Received 'Wait' APDU: {:?}\n", wait_response);
+
+    // testing authenticated commands
+
+    use secp256k1::ecdh::SharedSecret;
+    use secp256k1::hashes::sha256;
+    use secp256k1::rand;
+    use secp256k1::{Message, Secp256k1};
+
+    let secp = Secp256k1::new();
+    let (eseckey, epubkey) = secp.generate_keypair(&mut rand::thread_rng());
+    let message = Message::from_hashed_data::<sha256::Hash>("Hello World!".as_bytes());
+
+    let sig = secp.sign_ecdsa(&message, &eseckey);
+    assert!(secp.verify_ecdsa(&message, &sig, &epubkey).is_ok());
+
+    let s = Secp256k1::new();
+    let (sk1, pk1) = s.generate_keypair(&mut rand::thread_rng());
+    let (sk2, pk2) = s.generate_keypair(&mut rand::thread_rng());
+    let sec1 = SharedSecret::new(&pk2, &sk1);
+    let sec2 = SharedSecret::new(&pk1, &sk2);
+    assert_eq!(sec1, sec2);
+
+    // let ssk1 = SecretKey::from_slice(&sec1.secret_bytes()).expect("32 bytes, within curve order");
+    // let ssk2 = SecretKey::from_slice(&sec2.secret_bytes()).expect("32 bytes, within curve order");
+    // assert_eq!(ssk1,ssk2);
+    //
+    // let spk1 = PublicKey::from_secret_key(&secp, &ssk1);
+    // let spk2 = PublicKey::from_secret_key(&secp, &ssk2);
+    // assert_eq!(spk1,spk2);
+
+    // byte array xor
+    // let c: Vec<_> = a.iter().zip(b).map(|(x, y)| x ^ y).collect();
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,73 @@ pub const SELECT_CLA_INS_P1P2: [u8; 4] = [0x00, 0xA4, 0x04, 0x00];
 pub const CBOR_CLA_INS_P1P2: [u8; 4] = [0x00, 0xCB, 0x00, 0x00];
 
 // require nonce sizes (bytes)
-pub const CARD_NONCE_SIZE:usize = 16;
-pub const USER_NONCE_SIZE:usize = 16;
+pub const CARD_NONCE_SIZE: usize = 16;
+pub const USER_NONCE_SIZE: usize = 16;
+
+// Errors
+
+#[derive(Debug)]
+pub enum Error {
+    CiborDe(String),
+    CiborValue(String),
+    CkTap {
+        error: String,
+        code: usize,
+    },
+    #[cfg(feature = "pcsc")]
+    PcSc(String),
+}
+
+impl<T> From<ciborium::de::Error<T>> for Error
+where
+    T: core::fmt::Debug,
+{
+    fn from(e: ciborium::de::Error<T>) -> Self {
+        Error::CiborDe(e.to_string())
+    }
+}
+
+impl From<ciborium::value::Error> for Error {
+    fn from(e: ciborium::value::Error) -> Self {
+        Error::CiborDe(e.to_string())
+    }
+}
+
+#[cfg(feature = "pcsc")]
+impl From<pcsc::Error> for Error {
+    fn from(e: pcsc::Error) -> Self {
+        Error::PcSc(e.to_string())
+    }
+}
 
 // Apdu Traits
 
 pub trait CommandApdu {
-    fn apdu_bytes(&self) -> Vec<u8>;
+    fn apdu_bytes(&self) -> Vec<u8>
+    where
+        Self: serde::Serialize,
+    {
+        let mut command = Vec::new();
+        into_writer(&self, &mut command).unwrap();
+        build_apdu(&CBOR_CLA_INS_P1P2, command.as_slice())
+    }
 }
 
 pub trait ResponseApdu {
-    fn from_cbor<'a>(cbor: Vec<u8>) -> Self where Self: Deserialize<'a> + Debug {
-        let cbor_value: Value = from_reader(&cbor[..]).unwrap(); // TODO use Err
-        let cbor_struct: Self = cbor_value.deserialized().unwrap(); // TODO use Err
-        cbor_struct
+    fn from_cbor<'a>(cbor: Vec<u8>) -> Result<Self, Error>
+    where
+        Self: Deserialize<'a> + Debug,
+    {
+        let cbor_value: Value = from_reader(&cbor[..])?;
+        let cbor_struct: Result<ErrorResponse, _> = cbor_value.deserialized();
+        if let Ok(error_resp) = cbor_struct {
+            Err(Error::CkTap {
+                error: error_resp.error,
+                code: error_resp.code,
+            })?;
+        }
+        let cbor_struct: Self = cbor_value.deserialized()?;
+        Ok(cbor_struct)
     }
 }
 
@@ -37,13 +90,8 @@ fn build_apdu(header: &[u8], command: &[u8]) -> Vec<u8> {
 
 /// Applet Select
 ///
+#[derive(Default, Serialize, Clone, Debug, PartialEq, Eq)]
 pub struct AppletSelect {}
-
-impl Default for AppletSelect {
-    fn default() -> Self {
-        AppletSelect {}
-    }
-}
 
 impl CommandApdu for AppletSelect {
     fn apdu_bytes(&self) -> Vec<u8> {
@@ -55,43 +103,77 @@ impl CommandApdu for AppletSelect {
 ///
 #[derive(Serialize, Clone, Debug, PartialEq, Eq)]
 pub struct StatusCommand {
-    /// command
+    /// 'status' command
     cmd: String,
 }
 
 impl Default for StatusCommand {
     fn default() -> Self {
         StatusCommand {
-            cmd: "status".to_string()
+            cmd: "status".to_string(),
         }
     }
 }
 
-impl CommandApdu for StatusCommand {
-    fn apdu_bytes(&self) -> Vec<u8> {
-        let mut command = Vec::new();
-        into_writer(&self, &mut command).unwrap();
-        build_apdu(&CBOR_CLA_INS_P1P2, command.as_slice())
+impl CommandApdu for StatusCommand {}
+
+/// Read Command
+///
+/// Apps need to write a CBOR message to read a SATSCARD's current payment address, or a
+/// TAPSIGNER's derived public key.
+#[derive(Serialize, Clone, Debug, PartialEq, Eq)]
+pub struct ReadCommand {
+    /// 'read' command
+    cmd: String,
+    /// provided by app, cannot be all same byte (& should be random), 16 bytes
+    nonce: Vec<u8>,
+    /// (TAPSIGNER only) auth is required, 33 bytes
+    epubkey: Option<Vec<u8>>,
+    /// (TAPSIGNER only) auth is required encrypted CVC value, 6 to 32 bytes
+    xcvc: Option<Vec<u8>>,
+}
+
+impl CommandApdu for ReadCommand {}
+
+/// Wait Command
+///
+/// Invalid CVC codes return error 401 (bad auth), through the third incorrect attempt. After the
+/// third incorrect attempt, a 15-second delay is required. Any further attempts to authenticate
+/// will return error 429 (rate limited) until the delay has passed.
+///
+/// In rate-limiting mode, the status command returns the auth_delay field with a positive value.
+///
+/// The wait command takes one second to execute and reduces the auth_delay by one unit. Typically,
+/// 15 wait commands need to be executed before retrying a CVC.
+#[derive(Serialize, Clone, Debug, PartialEq, Eq)]
+pub struct WaitCommand {
+    /// 'wait' command
+    cmd: String,
+    /// app's ephemeral public key (optional)
+    epubkey: Option<Vec<u8>>,
+    /// encrypted CVC value (optional), 6 to 32 bytes
+    xcvc: Option<Vec<u8>>,
+}
+
+impl WaitCommand {
+    pub fn new(epubkey: Option<Vec<u8>>, xcvc: Option<Vec<u8>>) -> Self {
+        WaitCommand {
+            cmd: "wait".to_string(),
+            epubkey,
+            xcvc,
+        }
     }
 }
 
-// Read Command
-//
-// Apps need to write a CBOR message to read a SATSCARD's current payment address, or a
-// TAPSIGNER's derived public key.
-// #[derive(Serialize, Clone, Debug, PartialEq, Eq)]
-// pub struct ReadRequest {
-//     /// command
-//     cmd: String,
-//     /// provided by app, cannot be all same byte (& should be random)
-//     nonce: [u8;16],
-//     /// (TAPSIGNER only) auth is required
-//     epubkey: Option<[u8;33]>,
-//     /// (TAPSIGNER only) auth is required encrypted CVC value
-//     xcvc: Option<[u8;32]>,
-// }
+impl CommandApdu for WaitCommand {}
 
 // Responses
+
+#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct ErrorResponse {
+    error: String,
+    code: usize,
+}
 
 #[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct StatusResponse {
@@ -109,24 +191,40 @@ pub struct StatusResponse {
     #[serde(with = "serde_bytes")]
     card_nonce: Vec<u8>,
     testnet: Option<bool>,
+    auth_delay: Option<usize>,
 }
 
 impl ResponseApdu for StatusResponse {}
 
-// Read Response
-//
-// The signature is created from the digest (SHA-256) of these bytes:
-//
-// b'OPENDIME' (8 bytes)
-// (card_nonce - 16 bytes)
-// (nonce from read command - 16 bytes)
-// (slot - 1 byte)
-// #[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
-// pub struct ReadResponse {
-//     /// signature over a bunch of fields using private key of slot
-//     sig: [u8;64],
-//     /// public key for this slot/derivation
-//     pubkey: [u8;32],
-//     /// new nonce value, for NEXT command (not this one)
-//     card_nonce: [u8;16],
-// }
+/// Read Response
+///
+/// The signature is created from the digest (SHA-256) of these bytes:
+///
+/// b'OPENDIME' (8 bytes)
+/// (card_nonce - 16 bytes)
+/// (nonce from read command - 16 bytes)
+/// (slot - 1 byte)
+#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct ReadResponse {
+    /// signature over a bunch of fields using private key of slot, 64 bytes
+    sig: Vec<u8>,
+    /// public key for this slot/derivation, 33 bytes
+    pubkey: Vec<u8>,
+    /// new nonce value, for NEXT command (not this one), 16 bytes
+    card_nonce: Vec<u8>,
+}
+
+impl ResponseApdu for ReadResponse {}
+
+/// Wait Response
+///
+/// When auth_delay is zero, the CVC can be retried and tested without side effects.
+#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct WaitResponse {
+    /// command result
+    success: bool,
+    /// how much more delay is now required
+    auth_delay: usize,
+}
+
+impl ResponseApdu for WaitResponse {}
